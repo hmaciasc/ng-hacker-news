6729b95c12f3227b2c85a754b8cb9d5e6dfa1c7b
Héctor Macías Casado <hectormaciasc@gmail.com>
2017-06-07 22:58:07 +0200

    Item fetch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MomentModule } from 'angular2-moment';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -7,18 +11,25 @@ import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
 
+import { HackerNewsAPIService } from './hackernews-api.service';
+import { DomainPipe } from './domain.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     StoriesComponent,
     FooterComponent,
-    ItemComponent
+    ItemComponent,
+    DomainPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MomentModule
   ],
-  providers: [],
+  providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
