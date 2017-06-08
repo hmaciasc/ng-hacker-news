bec471ac587222368c629fe9360e2cf22b6eacf4
Héctor Macías Casado <hectormaciasc@gmail.com>
2017-06-08 16:55:44 +0200

    Refactoring stories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MomentModule } from 'angular2-moment';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +11,6 @@ import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
 
 import { HackerNewsAPIService } from './hackernews-api.service';
-import { DomainPipe } from './domain.pipe';
 
 @NgModule({
   declarations: [
@@ -20,14 +18,12 @@ import { DomainPipe } from './domain.pipe';
     HeaderComponent,
     StoriesComponent,
     FooterComponent,
-    ItemComponent,
-    DomainPipe
+    ItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    MomentModule
+    HttpModule
   ],
   providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -8,14 +8,11 @@ import { HackerNewsAPIService } from '../hackernews-api.service';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
-	@Input() itemID: number;
-  item;
+  @Input() item;
 
-  constructor(private _hackerNewsAPIService: HackerNewsAPIService) {}
+  constructor() {}
 
   ngOnInit() {
-    this._hackerNewsAPIService.fetchItem(this.itemID).subscribe(data => {
-      this.item = data;
-    }, error => console.log('Could not load item' + this.itemID));
+
   }
 }
